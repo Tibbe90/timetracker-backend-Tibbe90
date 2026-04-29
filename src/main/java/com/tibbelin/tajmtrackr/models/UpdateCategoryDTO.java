b849135f4a1e21dd2e18a17f1b972d6dc7d9cf99
Tibbe <tlin.jonas@gmail.com>
2026-04-29 11:58:28 +0200

    categoryController added, included slight authentication to simplify future implementation

---
+++ src/main/java/com/tibbelin/tajmtrackr/models/UpdateCategoryDTO.java
@@ -0,0 +1,14 @@
+package com.tibbelin.tajmtrackr.models;
+
+public class UpdateCategoryDTO {
+    
+    private String newName;
+
+    public String getNewName() {
+        return newName;
+    }
+    public void setNewName(String newName) {
+        this.newName = newName;
+    }
+
+}
