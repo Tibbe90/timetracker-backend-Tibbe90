731e0274f28d482d2387f99a9d350899e3472167
Tibbe <tlin.jonas@gmail.com>
2026-05-04 14:51:06 +0200

    updated CORS policy

--- src/main/java/com/tibbelin/tajmtrackr/configs/SecurityConfig.java
+++ src/main/java/com/tibbelin/tajmtrackr/configs/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.tibbelin.tajmtrackr.configs;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -8,6 +10,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 /******************************************************************************
  * Setting up CORS rules for spring security
  * https://docs.spring.io/spring-security/reference/servlet/integrations/cors.html
@@ -39,8 +43,21 @@ public class SecurityConfig {
         return http.build();
     }
 
+    // NOT USED
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+@Bean
+public UrlBasedCorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(Arrays.asList("http://127.0.0.1:5173", "http://localhost:5173/"));
+    configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+    configuration.setAllowedHeaders(Arrays.asList("*"));
+    configuration.setAllowCredentials(true);
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
+}
 }
